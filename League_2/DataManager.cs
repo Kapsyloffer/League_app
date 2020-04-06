b3ef33dfb8f392edf44fd195c403176c17a29a36
Christoffer <CrilleMega@gmail.com>
2020-04-06 10:18:48 +0200

    I'm Stuff

---
+++ League_2/DataManager.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace League_2
+{
+    class DataManager
+    {
+        //Håll all data här. Läs och skriv allt här.
+        private List<Player> PlayerList;
+        private Settings LocalSettings;
+        public List<Player> getPlayerList()
+        {
+            return PlayerList;
+        }
+        public void setPlayerList(List<Player> pl)
+        {
+            this.PlayerList = pl;
+        }
+        public void setSettings(Settings s)
+        {
+            this.LocalSettings = s;
+        }
+
+        public Settings GetSettings()
+        {
+            return LocalSettings;
+        }
+
+        public void LoadFile()
+        {
+
+        }
+
+        public void SaveFile()
+        {
+            
+        }
+    }
+}
---
+++ League_2/Player.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace League_2
+{
+    class Player
+    {
+        private String name;
+        private int ID;
+        public Player(String n, int ID)
+        {
+
+        }
+
+        public String getName()
+        {
+            return name;
+        }
+
+        public int getID()
+        {
+            return ID;
+        }
+    }
+}
